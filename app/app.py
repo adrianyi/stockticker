02a9d1d40ff213ed027bff6ee62f560abeb1ed4f
Adrian Yi <sjay87@gmail.com>
2018-03-06 16:14:48 +0000

    Quick fix: handle lowercase stock ticker names
    Other edits:
     - Changed input validation logic to give more specific error messages.
     - Removed unnecessary css/js links.

--- app/app.py
+++ app/app.py
@@ -64,32 +64,38 @@ def create_plot(data, values, ticker, width=800, height=500):
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
+    if request.method == 'POST':
         # Validate all input
-        ticker, values = None, None
-        if 'ticker' in request.form and request.form['ticker'] in codes:
+        ticker, values, err_msg = None, None, None
+        if 'ticker' in request.form and request.form['ticker'].upper() in codes:
             ticker = request.form['ticker'].upper()
         if 'values' in request.form:
-            values = request.form.getlist('values')
-            for value in values:
-                if value not in ['open', 'close', 'high', 'low']:
-                    values = None
-                    break
-            if values is not None and 'adjusted' in request.form:
+            values = [v.lower() for v in request.form.getlist('values')]
+            values = [v for v in values if v in ['open', 'close', 'high', 'low']]
+            if values and 'adjusted' in request.form:
                 for i, value in enumerate(values):
                     values[i] = 'adj_' + value
-        if ticker is None or values is None:
-            err_msg = 'Input error. Try again with valid ticker symbol and parameters.'
+
+        if ticker and values:
+            # Return plot if inputs are valid
+            data = get_stock_info(ticker)
+            p = create_plot(data, values, ticker)
+            script, div = components(p)
+            return render_template('index.html',
+                                   plot_div=div.strip(),
+                                   plot_script=script.strip())
+        else:
+            # Return error message if inputs are not valid
+            if ticker:
+                err_msg = 'Uh-oh. I did not recognize that ticker symbol. Please try again with a valid input.'
+            elif values:
+                err_msg = 'Please select at least one value you would like to plot.'
+            else:
+                err_msg = 'Input error. Try again with valid ticker symbol and parameters.'
             return render_template('index.html', error_message=err_msg)
 
-        data = get_stock_info(ticker)
-        p = create_plot(data, values, ticker)
-        script, div = components(p)
-        return render_template('index.html', plot_div=div.strip(), plot_script=script.strip())
     else:
-        return "Forbidden method"
+        return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
